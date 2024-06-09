73c5e1307b3afb9f825c713ed50aa75d4db7f88a
klu_2200031618 <kk258786@gmail.com>
2024-06-09 15:52:09 +0530

    A view of logic has been added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,15 @@
 import './allPages.css'
 
-export const Home=()=>{
+export const Home=()=>{  
+
     return(
         <div className="home_main_container">
-            Hi
+
+            <img className='cat_gif_holder_left' id='cat1' src={require('../gifs/cat1-scissor.gif')} alt='cat-gif'/>
+            <img className='cat_gif_holder_right' id='cat2' src={require('../gifs/cat2-rock.gif')} alt='cat-gif'/>
+
+            <img className='cat_image_holder_left' src={require('../images/cat1-scissors.png')} alt='cat-gif'/>
+            <img className='cat_image_holder_right' src={require('../images/cat2-rock.png')} alt='cat-gif'/>
         </div>
     )
 }
