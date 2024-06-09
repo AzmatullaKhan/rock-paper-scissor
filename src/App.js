183c900004b3a6156f6de45c61ed4637458274f1
klu_2200031618 <kk258786@gmail.com>
2024-06-09 12:28:33 +0530

    Required gifs has been cropped and need to add the logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import {Home} from './pages/home'
 
 function App() {
   return (
-    <div >
-      hi
-    </div>
+    <Home />
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+import './allPages.css'
+
+export const Home=()=>{
+    return(
+        <div className="home_main_container">
+            Hi
+        </div>
+    )
+}
