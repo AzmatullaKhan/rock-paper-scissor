3ce4f6148deea2c414c26845df9d2763955148e2
klu_2200031618 <kk258786@gmail.com>
2024-06-10 23:07:22 +0530

    Completed all necessary logic successfully, need to add some tomorrow

--- src/pages/home.js
+++ src/pages/home.js
@@ -45,9 +45,48 @@ export const Home=()=>{
 
     }
 
+    const getWinner=(number, string)=>{
+        let res=''
+        let footer_desc=document.getElementById('footer_description')
+        document.getElementById('footer_instruction').style.opacity=1
+        if(number===1)
+            res="paper"
+        if(number===2)
+            res="rock"
+        if(number===3)
+            res="scissor"
+
+        if(string === "paper"){
+            if(res==="scissor")
+                footer_desc.innerText='😈😈😈😈 You Insolent fool, I won. 😈😈😈😈'
+            else if(res===string)
+                footer_desc.innerText='😯😯😯😯 You were so close to Win. 😯😯😯😯'
+            else
+                footer_desc.innerText="🤬🤬🤬🤬 You won. I won't leave you next time... 🤬🤬🤬🤬"
+        }
+        else if(string === "rock"){
+            if(res === "paper")
+                footer_desc.innerText='😈😈😈😈 You Insolent fool, I won. 😈😈😈😈'
+            else if(res===string)
+                footer_desc.innerText='😯😯😯😯 You were so close to Win. 😯😯😯😯'
+            else   
+                footer_desc.innerText="🤬🤬🤬🤬 You won. I won't leave you next time... 🤬🤬🤬🤬"
+        }
+        else{
+            if(res==="rock")
+                footer_desc.innerText='😈😈😈😈 You Insolent fool, I won. 😈😈😈😈'
+            else if(res===string)
+                footer_desc.innerText='😯😯😯😯 You were so close to Win. 😯😯😯😯'
+            else
+                footer_desc.innerText="🤬🤬🤬🤬 You won. I won't leave you next time... 🤬🤬🤬🤬"
+        }
+    }
+
     const handleRockClick=()=>{
 
         let random_number=(Math.floor((Math.random() * 3) + 1))
+
+        document.getElementById('button_section').className='hidden'
 
         let main_container=document.getElementById('home_main_container')
         if(!firstClick){
@@ -78,11 +117,17 @@ export const Home=()=>{
         main_container.appendChild(hidden_image)
         main_container.appendChild(com_gif)
         main_container.appendChild(com_image)
+
+        setTimeout(()=>{
+            getWinner(random_number, "rock")
+        }, 2000)
     }
     
     const handlePaperClick=()=>{
 
         let random_number=(Math.floor((Math.random() * 3) + 1))
+
+        document.getElementById('button_section').className='hidden'
 
         let main_container=document.getElementById('home_main_container')
         if(!firstClick){
@@ -111,6 +156,10 @@ export const Home=()=>{
         let com_gif=generateComputerGif(random_number)
         let com_image=generateComputerImage(random_number)
 
+        setTimeout(()=>{
+            getWinner(random_number, "paper")
+        }, 2000)
+
         main_container.appendChild(gif_container)
         main_container.appendChild(hidden_image)
         main_container.appendChild(com_gif)
@@ -121,6 +170,8 @@ export const Home=()=>{
     const handleScissorClick=()=>{
 
         let random_number=(Math.floor((Math.random() * 3) + 1))
+
+        document.getElementById('button_section').className='hidden'
         
         let main_container=document.getElementById('home_main_container')
         if(!firstClick){
@@ -149,6 +200,10 @@ export const Home=()=>{
         let com_gif=generateComputerGif(random_number)
         let com_image=generateComputerImage(random_number)
 
+        setTimeout(()=>{
+            getWinner(random_number, "scissor")
+        }, 2000)
+
         main_container.appendChild(gif_container)
         main_container.appendChild(hidden_image)
         main_container.appendChild(com_gif)
@@ -158,10 +213,18 @@ export const Home=()=>{
     return(
         <div className="home_main_container" id='home_main_container'>
 
-            <button onClick={handleRockClick}>Rock</button>
-            <button onClick={handlePaperClick}>Paper</button>
-            <button onClick={handleScissorClick}>scissor</button>
-
+            <div id='button_section'>
+                <button onClick={handleRockClick}>Rock</button>
+                <button onClick={handlePaperClick}>Paper</button>
+                <button onClick={handleScissorClick}>scissor</button>
+            </div>
+            <footer>
+                <div style={{display:"flex"}}>
+                    <h1 id='footer_description'>Select one of them to play ROCK-PAPER-SCISSOR</h1><img src={require('../images/logo.png')} alt='logo' className='logo-image'/>
+                </div>
+                <h2 style={{margin:0,opacity:0}} id='footer_instruction'>🔔🔔Press Enter to try again🔔🔔</h2>
+                <p><a href="https://github.com/AzmatullaKhan">Copyright © All Rights Reserved 2024 Azmatulla Khan ®</a></p>
+            </footer>
         </div>
     )
 }
